63ab27c0ff0958e24d7d80a546f2959cc9fd1ae7
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-10 23:41:50 +0100

    Update Firebase configuration with project details
    Updated Firebase configuration with new project details.

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// PASTE YOUR FIREBASE CONFIG HERE FROM FIREBASE CONSOLE
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBwdhpv7e0Y3xNBovOpJEpRn9_jmDUOq8E",
+  authDomain: "namtls-voting-2026-2027.firebaseapp.com",
+  projectId: "namtls-voting-2026-2027",
+  storageBucket: "namtls-voting-2026-2027.firebasestorage.app",
+  messagingSenderId: "955792311858",
+  appId: "1:955792311858:web:49a566b351ebab86c05e2b",
+  measurementId: "G-SVDG83TZSG"
 };
 
 const app = initializeApp(firebaseConfig);
