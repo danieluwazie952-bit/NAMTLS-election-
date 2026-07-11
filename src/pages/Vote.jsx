46ce0b3e332b3a4e73eabff5306a409ecd778ede
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-11 02:16:38 +0100

    Delete src/pages/Vote.jsx

--- src/pages/Vote.jsx
+++
@@ -1,7 +0,0 @@
-export default function Vote() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Voting Page</h1>
-    </div>
-  )
-}
