fc1b107b832c9ca534a12c71af7f13b40fa4a74e
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-11 18:51:20 +0100

    Fix Vite config by adding base line

--- Vite.config.js
+++ Vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // <-- THIS IS THE FIX
+  base: '/',  // <-- ADD THIS LINE
   build: {
-    chunkSizeWarningLimit: 1000 // 1000kb instead
+    chunkSizeWarningLimit: 1000
   }
 })
