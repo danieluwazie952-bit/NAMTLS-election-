3abf24d832f18d9e8a20122eca44a8914ac8a27d
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-11 09:02:25 +0100

    Delete src/pages/AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++
@@ -1,7 +0,0 @@
-export default function AdminDashboard() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
-    </div>
-  )
-}
