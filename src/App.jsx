8e174e84c8d668d479b5a82f17fa342f289128ed
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-12 15:14:35 +0100

    Log routes loaded in App component
    Added console log for route setup.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Landing from './pages/Landing';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/StudentDashboard';
 
+console.log('ROUTES_LOADED: Setting up routes...');
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="/student/dashboard" element={<StudentDashboard />} />
     </Routes>
