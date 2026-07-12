292d6b193c8afd67a90acf3606a04ea272d173c0
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-12 18:53:26 +0100

    Simplify Vite configuration by removing jsxRuntime

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    react({
-      jsxRuntime: 'automatic', // <- changed
-    }),
-  ],
+  plugins: [react()],
 })
