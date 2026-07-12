d66e5115be0fe3de26d4b859609a5b8cf289b572
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-12 09:51:29 +0100

    Update Vite config to remove emptyOutDir option
    Removed 'emptyOutDir: true' from build configuration.

--- Vite.config.js
+++ Vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // This is the key. NOT './'
-  build: { outDir: 'dist', emptyOutDir: true }
+  base: '/',
+  build: { outDir: 'dist' }
 })
