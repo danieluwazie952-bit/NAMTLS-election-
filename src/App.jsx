bc97d7e681310a59b377ab0248456032c5c261b8
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-12 10:14:32 +0100

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <h1 style={{color: 'red', textAlign: 'center', marginTop: '100px'}}>NAMTLS IS LIVE</h1>
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminLogin from './pages/AdminLogin';
+import Landing from './pages/Landing';
+import AdminDashboard from './pages/AdminDashboard';
+import StudentDashboard from './pages/StudentDashboard';
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
+export default App;
