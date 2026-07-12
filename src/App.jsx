9a8dab71457f86f7bc945e753dbdbce4788ee16c
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-12 14:30:37 +0100

    Refactor routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AdminLogin from './pages/AdminLogin';
 import Landing from './pages/Landing';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/StudentDashboard';
-import './index.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/student-dashboard" element={<StudentDashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
+    </Routes>
   );
 }
+
 export default App;
