b870af3a6bd72f7fce57fbf0e9ea66bf721baeea
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-12 15:45:58 +0100

    Add Watermark component to App routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import AdminLogin from './pages/AdminLogin';
 import Landing from './pages/Landing';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/StudentDashboard';
+import Watermark from './components/Watermark';
 
 console.log('ROUTES_LOADED: Setting up routes...');
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/admin" element={<AdminLogin />} />
-      <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="/student/dashboard" element={<StudentDashboard />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/student/dashboard" element={<StudentDashboard />} />
+      </Routes>
+      <Watermark />
+    </>
   );
 }
 
