6fe08bd0c136b571d3ce22ab8cea36e39271bd71
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-13 19:30:54 +0100

    Add StudentLogin and StudentDashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AdminLogin from './pages/AdminLogin';
 import Landing from './pages/Landing';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/StudentDashboard';
-import StudentLogin from './pages/StudentLogin'; // <-- ADDED THIS LINE
+import StudentLogin from './pages/StudentLogin';
 import Watermark from './components/Watermark';
 
 console.log('ROUTES_LOADED: Setting up routes...');
@@ -14,10 +14,10 @@ function App() {
       <Watermark />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/student-login" element={<StudentLogin />} />
+        <Route path="/student" element={<StudentDashboard />} />
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/student-login" element={<StudentLogin />} /> {/* <-- ADDED THIS LINE */}
-        <Route path="/student-dashboard" element={<StudentDashboard />} />
       </Routes>
     </>
   );
