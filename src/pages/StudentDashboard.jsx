168e9c052784e4c9fcd68acaf4b83afa464bbbcb
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-14 01:50:59 +0100

    Remove footer text from StudentDashboard
    Removed the footer text 'Authorized and Verified by Meta EC' from the StudentDashboard component.

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -133,7 +133,6 @@ export default function StudentDashboard() {
             ))}
           </div>
         )}
-        <p style={{ marginTop: '32px', fontSize: '11px', textAlign: 'center', opacity: '0.5' }}>Authorized and Verified by Meta EC</p>
       </div>
     </div>
   );
