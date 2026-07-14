8339142e084f09859df762007300f2784c9d6c94
danieluwazie952-bit <danieluwazie952@gmail.com>
2026-07-14 02:10:06 +0100

    Remove unused Navigate import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import StudentLogin from './pages/StudentLogin';
 import StudentDashboard from './pages/StudentDashboard';
